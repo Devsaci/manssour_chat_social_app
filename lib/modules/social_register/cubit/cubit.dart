eb6b648e0f56da8715e95dadb46034f5d6c4488d
saci zakaria <sacideveloppeur@gmail.com>
2022-06-08 23:05:35 +0200

    SocialLoginModel? loginModel;

--- lib/modules/social_register/cubit/cubit.dart
+++ lib/modules/social_register/cubit/cubit.dart
@@ -2,8 +2,11 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:manssour_chat_social_app/modules/social_register/cubit/states.dart';
 
+import '../../../models/login_model.dart';
+
 class SocialRegisterCubit extends Cubit<SocialRegisterStates> {
   SocialRegisterCubit() : super(SocialRegisterInitialState());
 
   static SocialRegisterCubit get(context) => BlocProvider.of(context);
+  SocialLoginModel? loginModel;
 }
