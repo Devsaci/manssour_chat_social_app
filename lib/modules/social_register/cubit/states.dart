ba7c338849d139375897c7e2fdef7eb57fcd4e36
saci zakaria <sacideveloppeur@gmail.com>
2022-06-08 22:53:50 +0200

    class ShopRegisterSuccessState extends ShopRegisterStates {}

--- lib/modules/social_register/cubit/states.dart
+++ lib/modules/social_register/cubit/states.dart
@@ -4,10 +4,10 @@ class ShopRegisterInitialState extends ShopRegisterStates {}
 
 class ShopRegisterLoadingState extends ShopRegisterStates {}
 
+class ShopRegisterSuccessState extends ShopRegisterStates {}
+
 class ShopRegisterErrorState extends ShopRegisterStates {
   final String error;
 
   ShopRegisterErrorState(this.error);
 }
-
-
