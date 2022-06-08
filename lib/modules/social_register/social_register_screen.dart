8eef479294d8be6efa8edc2379ef50c66a1e8bb8
saci zakaria <sacideveloppeur@gmail.com>
2022-06-08 21:18:46 +0200

    Center

--- lib/modules/social_register/social_register_screen.dart
+++ lib/modules/social_register/social_register_screen.dart
@@ -15,90 +15,92 @@ class SocialRegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Form(
-            key: formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'REGISTER',
-                  style: Theme.of(context).textTheme.headline4?.copyWith(
-                        color: Colors.black,
-                      ),
-                ),
-                Text(
-                  'Register now to communicate with friends',
-                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                        color: Colors.grey,
-                      ),
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                defaultFormField(
-                  controller: nameController,
-                  type: TextInputType.name,
-                  validate: (value) {
-                    if (value!.isEmpty) {
-                      return 'please enter your name';
-                    }
-                    return null;
-                  },
-                  label: 'User Name',
-                  prefix: Icons.person,
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                defaultFormField(
-                  controller: emailController,
-                  type: TextInputType.emailAddress,
-                  validate: ( value) {
-                    if (value!.isEmpty) {
-                      return 'please enter your email address';
-                    }
-                  },
-                  label: 'Email Address',
-                  prefix: Icons.email_outlined,
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                defaultFormField(
-                  controller: passwordController,
-                  type: TextInputType.visiblePassword,
-                  suffix:null,
-                  onSubmit: (value) {},
-                  isPassword: false,
-                  suffixPressed: () {
-                  },
-                  validate: ( value) {
-                    if (value!.isEmpty) {
-                      return 'password is too short';
-                    }
-                  },
-                  label: 'Password',
-                  prefix: Icons.lock_outline,
-                ),
-                defaultFormField(
-                  controller: phoneController,
-                  type: TextInputType.phone,
-                  validate: ( value) {
-                    if (value!.isEmpty) {
-                      return 'please enter your phone number';
-                    }
-                  },
-                  label: 'Phone',
-                  prefix: Icons.phone,
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'REGISTER',
+                    style: Theme.of(context).textTheme.headline4?.copyWith(
+                          color: Colors.black,
+                        ),
+                  ),
+                  Text(
+                    'Register now to communicate with friends',
+                    style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                          color: Colors.grey,
+                        ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  defaultFormField(
+                    controller: nameController,
+                    type: TextInputType.name,
+                    validate: (value) {
+                      if (value!.isEmpty) {
+                        return 'please enter your name';
+                      }
+                      return null;
+                    },
+                    label: 'User Name',
+                    prefix: Icons.person,
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  defaultFormField(
+                    controller: emailController,
+                    type: TextInputType.emailAddress,
+                    validate: ( value) {
+                      if (value!.isEmpty) {
+                        return 'please enter your email address';
+                      }
+                    },
+                    label: 'Email Address',
+                    prefix: Icons.email_outlined,
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  defaultFormField(
+                    controller: passwordController,
+                    type: TextInputType.visiblePassword,
+                    suffix:null,
+                    onSubmit: (value) {},
+                    isPassword: false,
+                    suffixPressed: () {
+                    },
+                    validate: ( value) {
+                      if (value!.isEmpty) {
+                        return 'password is too short';
+                      }
+                    },
+                    label: 'Password',
+                    prefix: Icons.lock_outline,
+                  ),
+                  defaultFormField(
+                    controller: phoneController,
+                    type: TextInputType.phone,
+                    validate: ( value) {
+                      if (value!.isEmpty) {
+                        return 'please enter your phone number';
+                      }
+                    },
+                    label: 'Phone',
+                    prefix: Icons.phone,
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
 
-              ],
+                ],
+              ),
             ),
           ),
         ),
