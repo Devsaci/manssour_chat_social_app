e24f20383a3a87fc9ef47691c71255dae8f1589c
saci zakaria <sacideveloppeur@gmail.com>
2022-06-08 23:03:56 +0200

    static SocialRegisterCubit get(context) => BlocProvider.of(context);

--- lib/modules/social_register/cubit/cubit.dart
+++ lib/modules/social_register/cubit/cubit.dart
@@ -1,6 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:manssour_chat_social_app/modules/social_register/cubit/states.dart';
 
 class SocialRegisterCubit extends Cubit<SocialRegisterStates> {
   SocialRegisterCubit() : super(SocialRegisterInitialState());
+
+  static SocialRegisterCubit get(context) => BlocProvider.of(context);
 }
